6942edf1a1433da75a1f8394a4e81ce75f4593e1
Adrian Ezequiel Gutierrez <adrian.gutierrezo@iteso.mx>
2022-10-10 12:19:09 -0500

    Nueva modificación

--- hello.c
+++ hello.c
@@ -6,5 +6,6 @@ int main(int argc, char** argv)
     printf("Hola Mundo");
     printf("Primera modificación");
     printf("Nueva modificación la primera en GITHUB");
+    printf("El total es %d", 1 + 1);
     return(EXIT_SUCCESS);
 }
